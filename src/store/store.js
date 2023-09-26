aa35511becf383f60df06528311bf81b9b5ccd26
Mari Vaypane <avemari98@gmail.com>
2023-09-26 23:21:59 +0300

    create store

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+ import { combineReducers, createStore } from 'redux';
+ import { projectsList } from '../reducers/projectsListReducer'
+
+ const rootReducers = combineReducers({
+    projectsList
+ });
+
+ export const store = createStore(rootReducers)
