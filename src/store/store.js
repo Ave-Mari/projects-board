29921c74b5ecff4c781aec0f17fe83733d438faf
Mari Vaypane <avemari98@gmail.com>
2023-09-28 00:20:50 +0300

    add devtools to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { projectsList } from "../reducers/projectsListReducer";
 
 const rootReducers = combineReducers({
-  projectsList,
+  projectsList
 });
 
-export const store = createStore(rootReducers);
+export const store = createStore(rootReducers, composeWithDevTools());
