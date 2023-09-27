b0c8223fb9205ab8f0c4814dfa05c38224405a8e
Mari Vaypane <avemari98@gmail.com>
2023-09-27 15:35:58 +0300

    add browser router to page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App.jsx'
-import './index.css'
-import {store} from './store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import './index.scss'
+import { store } from "./store/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
