d36ea468059c309abb76f7f7c222dc3c5d64da2b
Mari Vaypane <avemari98@gmail.com>
2023-09-28 00:24:04 +0300

    clear from useless code

--- src/pages/ProjectItemPage.jsx
+++ src/pages/ProjectItemPage.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import Board from '../components/Board/Board'
 
 export default function ProjectItemPage() {
+ 
   return (
     <section>
-      <Board status="Queue" children={
-        <p>lfkjgorfhjk</p>} 
+      <Board status="Queue" 
       />
-      <Board status="Development" children={
-        <p>lffgorfhjk</p>} 
+      <Board status="Development" 
       />
-      <Board status="Done" children={
-        <p>lfkjgorfhjk</p>} 
+      <Board status="Done" 
       />
     </section>
   )
