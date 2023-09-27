111e41315cf3c571d1dd71da05cdf73d7e88b875
Mari Vaypane <avemari98@gmail.com>
2023-09-28 00:31:19 +0300

    add function to map tasks on boards

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {useSelector} from 'react-redux';
 
-export default function Board({ status, children }) {
+
+export default function Board({ status  }) {
+  const projectTasksList = useSelector(state => state[tasks]);
+  console.log(projectTasksList)
+
   return (
-    <div>
+    <div className='board'>
         {status}
-        {children}
+        <div className='board-wrapper'>
+          {projectTasksList.map((item) => {
+            const {taskId, headline, description, status} = item;
+            return (
+              <TaskItem
+                key={taskId}
+                headline={headline}
+                description={description}
+                status={status}
+                />
+            )
+          })}
+
+        </div>
+     
+
     </div>
   )
 }
