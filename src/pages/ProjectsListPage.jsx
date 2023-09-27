b33fcf17bc1e6cfc938fefff29598e061998f915
Mari Vaypane <avemari98@gmail.com>
2023-09-27 21:15:48 +0300

    add modal to page

--- src/pages/ProjectsListPage.jsx
+++ src/pages/ProjectsListPage.jsx
@@ -1,18 +1,55 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Modal from '../components/Modal/Modal'
+import { addProject } from "../actions/actions";
+import Modal from "../components/Modal/Modal";
 
 export default function ProjectsListPage() {
+  const dispatch = useDispatch();
+
   const [modalVisible, setModalVisible] = useState(false);
+  const [projectTitle, setProjectTitle] = useState("");
+
   const projectsList = useSelector((state) => state.projectsList);
+
+  console.log(projectsList)
+
+  const handleInputChange = (e) => {
+    setProjectTitle(e.target.value);
+  };
+
+  const handleSubmitForm = () => {
+    dispatch(addProject(projectTitle));
+    setProjectTitle("");
+    setModalVisible(false);
+  };
 
   const openModal = (e) => {
     e.preventDefault();
     setModalVisible(true);
   };
+
   return (
     <section>
       <button onClick={openModal}>Create new project</button>
+
+      {modalVisible && (
+        <Modal
+          //closeModal={setModalVisible(false)}
+          //modalVisible={modalVisible}
+          //setModalVisible={setModalVisible}
+          children={
+            <form onSubmit={handleSubmitForm}>
+              <input
+                type="text"
+                value={projectTitle}
+                placeholder="Project name"
+                onChange={handleInputChange}
+              />
+              <button type="submit">Create project</button>
+            </form>
+          }
+        />
+      )}
 
       {projectsList.map((item) => {
         const { title, projectId } = item;
