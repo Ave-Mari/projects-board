eed04cf2f31443709ce575c516075517cf07ef22
Mari Vaypane <avemari98@gmail.com>
2023-09-27 15:20:54 +0300

    add routes and components to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+//components
+import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import ProjectsListPage from "./pages/ProjectsListPage";
+//styles
+import "./index.module.scss";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      hello
-    </>
-  )
+    <main>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/:projectId" element={<ProjectsListPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+
