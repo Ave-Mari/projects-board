1ea8b3705f89754f9b3e1dcb746e6127b381fdf4
Mari Vaypane <avemari98@gmail.com>
2023-09-27 20:04:51 +0300

    create initial state and reducers

--- src/reducers/projectsListReducer.js
+++ src/reducers/projectsListReducer.js
@@ -1,3 +1,20 @@
-const initialState = [];
+const initialState = [
+    {
+        projectId: 142354,
+        title: 'Example Project',
+        tasks: []
+    }
+]
 
-export const projectsList = (state = initialState) => {return null}
+export const projectsList = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_PROJECT':
+            return {
+                ...state, 
+                projectsList: [state.projectsList, action.payload]
+            }
+        default:
+            return state;
+    }
+}
+
