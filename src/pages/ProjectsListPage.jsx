93f1039b2ce159b67f4635a1a0fb2c419641fc8d
Mari Vaypane <avemari98@gmail.com>
2023-09-27 20:04:27 +0300

    add modal and map projects list to page

--- src/pages/ProjectsListPage.jsx
+++ src/pages/ProjectsListPage.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Modal from '../components/Modal/Modal'
 
 export default function ProjectsListPage() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const projectsList = useSelector((state) => state.projectsList);
+
+  const openModal = (e) => {
+    e.preventDefault();
+    setModalVisible(true);
+  };
   return (
     <section>
+      <button onClick={openModal}>Create new project</button>
 
+      {projectsList.map((item) => {
+        const { title, projectId } = item;
+        return (
+          <ul>
+            <li key={projectId}>{title}</li>
+          </ul>
+        );
+      })}
     </section>
   );
 }
