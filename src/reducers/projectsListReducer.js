9642018efeeb2e4f85eca9e64a8975f947f9e6cd
Mari Vaypane <avemari98@gmail.com>
2023-09-28 00:30:51 +0300

    add id to every task

--- src/reducers/projectsListReducer.js
+++ src/reducers/projectsListReducer.js
@@ -6,16 +6,19 @@ const initialState = [
         title: 'Example Project',
         tasks: [
             {
+                taskId: 214532,
                 headline: 'Create the design layout',
                 description: 'Create design in Figma and show it to client',
                 status: 'Development',                
             },
             {
+                taskId: 12645,
                 headline: 'Interviews',
                 description: 'Plan the interviews with potentional cadidates',
                 status: 'Queue',                
             },
             {
+                taskId: 67343,
                 headline: 'Purchases for office',
                 description: 'Discuss the purchases for office-space with manager',
                 status: 'Done',                
