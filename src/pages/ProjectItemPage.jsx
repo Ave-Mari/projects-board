2ea75096a42b8e7c136b028305b107d00c430693
Mari Vaypane <avemari98@gmail.com>
2023-09-28 12:07:00 +0300

    take params from url and use it to map tasks from specific project

--- src/pages/ProjectItemPage.jsx
+++ src/pages/ProjectItemPage.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import Board from '../components/Board/Board'
+import { projectsList } from '../reducers/projectsListReducer'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
-export default function ProjectItemPage() {
- 
+export default function ProjectItemPage({ children, projectsList }) { 
+  const { projectId } = useParams();
+  const projectsState = useSelector((state) => state.projectsList);
+  const project = projectsState.find((p) => p.projectId === Number(projectId))
+  const tasks = project.tasks;
+
+  console.log('tasks: ', tasks)
+
+  
   return (
     <section>
-      <Board status="Queue" 
-      />
-      <Board status="Development" 
-      />
-      <Board status="Done" 
-      />
+       {tasks.map((item) => {
+        const { taskId, headline, description, status } = item;
+        if (item.status === status) {
+          return (
+            <Board
+              key={projectId}
+              taskId={taskId}
+              headline={headline}
+              description={description}
+              status={status}
+            />
+          );
+        }
+      })} 
+      {children}
     </section>
-  )
+  );
 }
