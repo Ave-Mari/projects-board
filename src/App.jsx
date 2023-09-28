b3fceb760a8e5804718d7a1ac6db8975c92d3c01
Mari Vaypane <avemari98@gmail.com>
2023-09-28 12:03:17 +0300

    select state from store

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { projectsList } from './reducers/projectsListReducer'
 //components
 import Layout from "./pages/Layout";
 import ErrorPage from "./pages/ErrorPage";
@@ -9,12 +11,13 @@ import ProjectItemPage from "./pages/ProjectItemPage";
 import "./App.scss";
 
 export default function App() {
+  const projectsList = useSelector((state) => state.projectsList);
   return (
     <main>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="projects" element={<ProjectsListPage />} />
-          <Route path="/:projectId" element={<ProjectItemPage />} />
+          <Route path="projects/:projectId" element={<ProjectItemPage projectsList={projectsList} />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
