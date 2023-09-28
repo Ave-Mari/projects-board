cd71fb728ecefe923e76ce34bfe80451e000eaa7
Mari Vaypane <avemari98@gmail.com>
2023-09-29 00:34:18 +0300

    add onClick function to close modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Modal.scss'
 
-export default function Modal({children}) {
+export default function Modal({children, closeModal}) {
   return (
-    <div className="frame">
+    <div className="frame" onClick={closeModal}>
       <div className="modal-window">
        {children}
       </div>
