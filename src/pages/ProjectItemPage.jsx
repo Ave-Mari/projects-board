88a60bb12da1816e4b14da55fcad0d9162cc82cd
Mari Vaypane <avemari98@gmail.com>
2023-09-28 14:09:51 +0300

    add boards to project page

--- src/pages/ProjectItemPage.jsx
+++ src/pages/ProjectItemPage.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import Board from '../components/Board/Board'
-import { projectsList } from '../reducers/projectsListReducer'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import React from "react";
+import Board from "../components/Board/Board";
+import { useParams } from "react-router-dom";
+import { projectsList } from "../reducers/projectsListReducer";
+import { useSelector } from "react-redux";
 
-export default function ProjectItemPage({ children, projectsList }) { 
+export default function ProjectItemPage({ children }) {
   const { projectId } = useParams();
   const projectsState = useSelector((state) => state.projectsList);
-  const project = projectsState.find((p) => p.projectId === Number(projectId))
+  const project = projectsState.find((p) => p.projectId === Number(projectId));
   const tasks = project.tasks;
 
-  console.log('tasks: ', tasks)
+  if (!project) {
+    return <div>Project doesn't exist</div>;
+  }
 
-  
+  console.log(project.tasks);
+
   return (
     <section>
-       {tasks.map((item) => {
+      <h1>{project.title}</h1>
+      {tasks.map((item) => {
         const { taskId, headline, description, status } = item;
-        if (item.status === status) {
-          return (
-            <Board
-              key={projectId}
-              taskId={taskId}
-              headline={headline}
-              description={description}
-              status={status}
-            />
-          );
-        }
-      })} 
-      {children}
+        return (
+          <Board
+            taskId={taskId}
+            headline={headline}
+            description={description}
+            status={status}
+          />
+        );
+      })}
     </section>
   );
 }
