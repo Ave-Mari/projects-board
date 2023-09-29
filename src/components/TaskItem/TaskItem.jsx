4df31be2082751c357cbfbeb55fedaee0bad993c
Mari Vaypane <avemari98@gmail.com>
2023-09-30 00:56:28 +0300

    add task values to task item

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function TaskItem({ headline, description, status }) {
   return (
-    <div>
-        <h3>{headline}</h3>
-        <p>{description}</p>
-        <p>{status}</p>
-    </div>
-  )
+    <li>
+      <h3>{headline}</h3>
+      <p>{description}</p>
+      <p>{status}</p>
+    </li>
+  );
 }
