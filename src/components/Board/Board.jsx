95cbd9d775b2dd9caea1a4e2bc44b8bd13ec7ec6
Mari Vaypane <avemari98@gmail.com>
2023-09-30 00:56:05 +0300

    map tasks on boards

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { projectsList } from "../../reducers/projectsListReducer";
 //components
-import TaskItem from '../TaskItem/TaskItem'
+import TaskItem from "../TaskItem/TaskItem";
 
-export default function Board({ taskId, headline, description, status }) {
-
+export default function Board({ tasks, status }) {
   return (
     <div className="board">
       <h2>{status}</h2>
       <div className="board-wrapper">
-        <TaskItem
-          key={taskId}
-          headline={headline}
-          description={description}
-          status={status}
-        />
+        <ul>
+          {tasks.map((item) => {
+            const { taskId, headline, description, status } = item;
+            return (
+              <TaskItem
+                key={taskId}
+                headline={headline}
+                description={description}
+                status={status}
+              />
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
