d88fcabbfcf6931768698ff3426beb4ed2431112
Mari Vaypane <avemari98@gmail.com>
2023-09-30 00:55:52 +0300

    add board to empty project

--- src/pages/ProjectItemPage/ProjectItemPage.jsx
+++ src/pages/ProjectItemPage/ProjectItemPage.jsx
@@ -4,54 +4,49 @@ import { useParams, useNavigate } from "react-router-dom";
 import { projectsList } from "../../reducers/projectsListReducer";
 import { useSelector } from "react-redux";
 //styles
-import './ProjectItemPage.scss'
+import "./ProjectItemPage.scss";
 
 export default function ProjectItemPage() {
   const { projectId } = useParams();
   const projectsState = useSelector((state) => state.projectsList);
   const project = projectsState.find((p) => p.projectId === Number(projectId));
   const tasks = project.tasks;
-
+  const status = tasks.status;
+  console.log("status: ", status);
 
   if (!project) {
-    return <div><h2>Project doesn't exist</h2></div>;
+    return (
+      <div>
+        <h2>Project doesn't exist</h2>
+      </div>
+    );
   }
-
-  console.log('length:', tasks.length);
 
   return (
     <section>
       <h1>{project.title}</h1>
-      <div className="boards-wrapper">
-      {tasks.map((item) => {
-        const { taskId, headline, description, status } = item;
-        if (tasks.length > 0) {
-          return (
-            <Board
-              taskId={taskId}
-              headline={headline}
-              description={description}
-              status={status}
-            />
-          )
-        } else {
-          return (
-            <>
-            <Board
-              status="Queue"
-            />
-            <Board
-              status="Development"
-            />
-            <Board
-              status="Done"
-            />
-            </>
-          )
-        }
-        
-      })}
-      </div>     
+      {tasks.length === 0 ? (
+        <div className="boards-wrapper">
+          <Board status="Queue" tasks={[]} />
+          <Board status="Development" tasks={[]} />
+          <Board status="Done" tasks={[]} />
+        </div>
+      ) : (
+        <div className="boards-wrapper">
+          <Board
+            status="Queue"
+            tasks={tasks.filter((item) => item.status === "Queue")}
+          />
+          <Board
+            status="Development"
+            tasks={tasks.filter((item) => item.status === "Development")}
+          />
+          <Board
+            status="Done"
+            tasks={tasks.filter((item) => item.status === "Done")}
+          />
+        </div>
+      )}
     </section>
   );
 }
