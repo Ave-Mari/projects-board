0f75064be4fcf859fb8b50cee82b606c6b50ae78
Mari Vaypane <avemari98@gmail.com>
2023-09-30 02:30:28 +0300

    add modal to page

--- src/pages/ProjectItemPage/ProjectItemPage.jsx
+++ src/pages/ProjectItemPage/ProjectItemPage.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "../../components/Board/Board";
+import Modal from "../../components/Modal/Modal";
+import { addTask } from '../../actions/actions'
 import { useParams, useNavigate } from "react-router-dom";
 import { projectsList } from "../../reducers/projectsListReducer";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 //styles
 import "./ProjectItemPage.scss";
 
 export default function ProjectItemPage() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [task, setTask] = useState({
+    headline: "",
+    description: "",
+  });
+
+  const dispatch = useDispatch();
+
   const { projectId } = useParams();
   const projectsState = useSelector((state) => state.projectsList);
   const project = projectsState.find((p) => p.projectId === Number(projectId));
   const tasks = project.tasks;
-  const status = tasks.status;
-  console.log("status: ", status);
+
+  const createTaskModal = (e) => {
+    e.preventDefault();
+    setModalVisible(true);
+  };
+
+  const closeModal = (e) => {
+    if (e.target === e.currentTarget) {
+      setModalVisible(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setTask({...task, [name]: value})
+  }
+
+  const handleSubmit = () => {
+    const newTask = {
+      projectId: new Date().valueOf(),
+      headline: task.headline,
+      description: task.description,
+      status: 'Queue'
+    }
+    dispatch(addTask(projectId, newTask));
+    setTask({
+      headline: "",
+      description: "",
+    });
+    setModalVisible(false);
+  };
 
   if (!project) {
     return (
@@ -24,7 +64,32 @@ export default function ProjectItemPage() {
 
   return (
     <section>
+      {modalVisible && (
+        <Modal
+          closeModal={closeModal}
+          children={
+            <form onSubmit={handleSubmit}>
+              <input
+                onChange={handleChange}
+                type="text"
+                placeholder="Headline"
+                name="headline"
+                value={task.headline}
+              />
+              <input
+                onChange={handleChange}
+                type="text"
+                placeholder="Description"
+                name="description"
+                value={task.description}
+              />
+              <button className="btn-create" type="submit">Create Task</button>
+            </form>
+          }
+        />
+      )}
       <h1>{project.title}</h1>
+      <button onClick={createTaskModal} className="btn-create">Create Task</button>
       {tasks.length === 0 ? (
         <div className="boards-wrapper">
           <Board status="Queue" tasks={[]} />
