be01390610f22e2ad3d925cd0ea896e325fd904b
Mari Vaypane <avemari98@gmail.com>
2023-10-01 01:28:44 +0300

    add dnd provider to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import { projectsList } from './reducers/projectsListReducer'
+import { useSelector } from "react-redux";
+import { projectsList } from "./reducers/projectsListReducer";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 //components
 import Layout from "./pages/Layout/Layout";
 import ErrorPage from "./pages/ErrorPage";
@@ -12,18 +14,18 @@ import "./App.scss";
 
 export default function App() {
   const projectsList = useSelector((state) => state.projectsList);
- 
+
   return (
     <main>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="projects" element={<ProjectsListPage />} />
-          <Route path="projects/:projectId" element={<ProjectItemPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+      <DndProvider backend={HTML5Backend}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="projects" element={<ProjectsListPage />} />
+            <Route path="projects/:projectId" element={<ProjectItemPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </DndProvider>
     </main>
   );
 }
-
-
