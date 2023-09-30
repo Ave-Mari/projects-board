e444e67152c8df23e4bbc1e0f8caaf2e5d408753
Mari Vaypane <avemari98@gmail.com>
2023-10-01 00:51:07 +0300

    add classNames to elements

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+//styles
+import './TaskItem.scss'
 
 export default function TaskItem({ headline, description, status, date }) {
   return (
-    <li>
+    <li className={status === "Queue" ? "queue" : status === "Development" ? "development" : "done"}>
       <h3>{headline}</h3>
-      <p>{description}</p>
-      <p>{status}</p>
-      <p>{date}</p>
+      <p className="description">{description}</p>
+      <p className="status">{status}</p>
+      <p className="date">{date}</p>
     </li>
   );
 }
