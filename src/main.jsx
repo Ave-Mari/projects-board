1e4f5bf14df984d6b7f73a2cbf17712f947f2631
Mari Vaypane <avemari98@gmail.com>
2023-10-02 02:50:12 +0300

    add createStore and save state to localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { rootReducer } from "./store/store.js";
+import { loadState, saveState } from "./store/localstorage.js";
+import throttle from "lodash/throttle";
 import App from "./App.jsx";
-import './index.scss'
-import { store } from "./store/store.js";
+import "./index.scss";
+
+const persistedState = loadState();
+
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      projectsList: store.getState().projectsList,
+    });
+  }, 1000)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />  
+      <App />
     </BrowserRouter>
   </Provider>
 );
