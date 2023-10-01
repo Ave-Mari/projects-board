2a38215681f891a7eceb851e2ea5341d1738b8a7
Mari Vaypane <avemari98@gmail.com>
2023-10-02 02:49:08 +0300

    move store creation to main.js file

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,7 @@
 import { combineReducers } from "redux";
-import { legacy_createStore as createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { projectsList } from "../reducers/projectsListReducer";
 
-const rootReducers = combineReducers({
-  projectsList
+export const rootReducer = combineReducers({
+  projectsList: projectsList,
 });
 
-export const store = createStore(rootReducers, composeWithDevTools());
