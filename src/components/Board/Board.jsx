f2dfd6b29f38a40f4e9dcdcd4a4d118361688506
Mari Vaypane <avemari98@gmail.com>
2023-10-02 00:39:18 +0300

    add drop function to Board component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useDrop } from "react-dnd";
 import { projectsList } from "../../reducers/projectsListReducer";
+import { updateTaskStatus } from "../../actions/actions";
 //components
 import TaskItem from "../TaskItem/TaskItem";
 //styles
-import './Board.scss'
+import "./Board.scss";
 
 export default function Board({ tasks, status }) {
+  const dispatch = useDispatch();
+
+  const handleDrop = (taskId, newStatus) => {
+    dispatch(updateTaskStatus(taskId, newStatus));
+  };
+
+  const [, ref] = useDrop({
+    accept: "TASK",
+    drop: (item) => {
+      const taskId = item.taskId;
+      const newStatus = status;
+      handleDrop(taskId, newStatus);
+      console.log(
+        `Перетащена задача с ID: ${item.taskId} на доску "${status}"`
+      );
+    },
+  });
   return (
-    <div className="board">
+    <div
+      className={
+        status === "Queue"
+          ? "board-queue"
+          : status === "Development"
+          ? "board-development"
+          : "board-done"
+      }
+      ref={ref}
+    >
       <h2>{status}</h2>
       <div className="board-wrapper">
         <ul>
@@ -17,6 +45,7 @@ export default function Board({ tasks, status }) {
             return (
               <TaskItem
                 key={taskId}
+                taskId={taskId}
                 headline={headline}
                 description={description}
                 status={status}
