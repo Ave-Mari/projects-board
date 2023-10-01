6c3bccdf545d3eeeafad84c5c7e1e6ee6ce89bda
Mari Vaypane <avemari98@gmail.com>
2023-10-02 00:33:22 +0300

    add function to drag item

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 //styles
-import './TaskItem.scss'
+import "./TaskItem.scss";
 
-export default function TaskItem({ headline, description, status, date }) {
+export default function TaskItem({
+  taskId,
+  headline,
+  description,
+  status,
+  date,
+}) {
+  const [, ref] = useDrag({
+    type: "TASK",
+    item: { taskId: taskId },
+  });
   return (
-    <li className={status === "Queue" ? "queue" : status === "Development" ? "development" : "done"}>
+    <li
+      className={
+        status === "Queue"
+          ? "queue"
+          : status === "Development"
+          ? "development"
+          : "done"
+      }
+      ref={ref}
+    >
       <h3>{headline}</h3>
       <p className="description">{description}</p>
       <p className="status">{status}</p>
