411982a42cab9a08e63301a1bf94df3085c7f7ca
AveMari <avemari98@gmail.com>
2023-12-11 12:12:56 +0300

    put classname of modal to prop

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Modal.scss'
 
-export default function Modal({children, closeModal}) {
+export default function Modal({children, closeModal, modalClass}) {
   return (
     <div className="frame" onClick={closeModal}>
-      <div className="modal-window">
+      <div className={modalClass}>
        {children}
       </div>
     </div>
